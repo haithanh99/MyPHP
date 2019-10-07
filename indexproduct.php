ffe8c17a2a6aa2a63731c01705eb572106e2bc0e
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-10-07 15:55:19 +0700

    Update file indexproduct.php

--- indexproduct.php
+++ indexproduct.php
@@ -1,14 +1,29 @@
+
 <!DOCTYPE html>
 <html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/customcss.css">
+    <!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.0/css/mdb.min.css" rel="stylesheet">
+    <title>Product Database</title>
+  </head>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Product Page</h1>
 
 <?php
-echo "Show all rows from Postgres Database";
-
-//refere to database
-$db = parse_url(getenv("DATABASE_URL"));
+	echo "Showing all rows from Postgres Database";
+	
+	//Refere to database 
+	$db = parse_url(getenv("DATABASE_URL"));
 
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -18,23 +33,24 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-//you sql query
-$sql = "SELECT Name, Price FROM Products";
-$stmt = $pdo -> prepare ($sql);
-//execure  the query on the server and return the result
-$stmt -> setFetchMode(PDO::PETCH_ASSOC);
-$stmt -> execure();
-$resultSet = $stmt -> fetchAll();
-//display the data
+	//your sql query
+	$sql = "SELECT * FROM Product";
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+	//display the data 
 ?>
 <ul>
-<?php
-foreach ($resultSet as $row) {
-	echo "<li>".
-	$row["Name"].'--'.$row["Price"]."</li>";
-}
-?>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" .
+				$row["productID"] . '--'. $row["name"] . '--'. $row["price"]
+			. "</li>";
+		}
+	?>
 </ul>
-// echo "Hello World! teacher";
+
 </body>
 </html>
