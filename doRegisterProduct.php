69e89b474da32b5447bbf9d162f182d9b32712d0
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-10-09 15:59:56 +0700

    registerproduct.php

---
+++ doRegisterProduct.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Processing</title>
+</head>
+<body>
+	<?php 
+		$name = $_POST["txtName"];
+		$price = $_POST["txtPrice"];	
+		echo $name;
+		
+		//Refere to database 
+	   $db = parse_url(getenv("DATABASE_URL"));
+	   $pdo = new PDO("pgsql:" . sprintf(
+	        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	        $db["host"],
+	        $db["port"],
+	        $db["user"],
+	        $db["pass"],
+	        ltrim($db["path"], "/")
+	   ));
+	   $data = [
+		    'name' => $name,
+		    'price' => $course,
+		$stmt =  $pdo->prepare("INSERT INTO products(name, price) VALUES (:name,:price)");	
+		$stmt->execute($data);
+	 ?>
+	 <h2>Thank you <?php echo $name?>  for registering 
+	 		<?php echo $price?>
+	 </h2>
+	 <ul>
+	 	<li><?php echo $name?></li>
+	 	<li><?php echo $price?></li>
+
+	 </ul>
+	 <a href="indexproduct.php">Index</a>
+</body>
+</html>
---
+++ registerproduct.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="doRegisterProduct.php">
+	<table>
+		<tr>
+			<td>Name</td>
+			<td><input type="text" name="txtName"></td>
+		</tr>
+		<tr>
+			<td>Price</td>
+			<td><input type="text" name="txtPrice"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="Register"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
